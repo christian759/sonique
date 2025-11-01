c8b3e0d3a1189eacd49da7840f3c6a42f8b393e0
1nthe4byss <christian4onos@outlook.com>
2025-11-01 21:59:01 +0100

    Create JwtUtil

---
+++ src/main/java/com/csphere/sonique/security/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.csphere.sonique.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET = "secret_key_auron";
+    private final long EXPIRATION = 1000 * 60 * 60; // 1 hour
+
+    public String generateToken(String username, String role) {
+        return Jwts.builder()
+                .setSubject(username)
+                .claim("role", role)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser().setSigningKey(SECRET)
+                .parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+}
